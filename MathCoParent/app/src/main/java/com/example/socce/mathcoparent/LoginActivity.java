3a699ad2b68ababfcdc4d7534906a8087e917d52
meaadh <meaadhassan09@gmail.com>
2017-09-23 10:16:06 -0400

    Changes

--- MathCoParent/app/src/main/java/com/example/socce/mathcoparent/LoginActivity.java
+++ MathCoParent/app/src/main/java/com/example/socce/mathcoparent/LoginActivity.java
@@ -7,19 +7,14 @@ import android.widget.TextView;
 public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor>{
 
 
-
-    private AutoCompleteTextView mEmailView;
-        private EditText mPasswordView;
-        private View mProgressView;
-        private View mLoginFormView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
-        populateAutoComplete();
+
     /* Used to load the 'native-lib' library on application startup. */
     static {
         System.loadLibrary("native-lib");
     }
+
 }
